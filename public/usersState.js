ebd5490234882c8e264e9b99bf21a28f0bfc7fa6
Mael36 <mael.beltrame@free.fr>
2026-01-08 16:21:55 +0100

    Update usersState.js

--- public/usersState.js
+++ public/usersState.js
@@ -21,25 +21,28 @@ function saveUsers(users) {
   localStorage.setItem('usersState', JSON.stringify(users));
 }
 
+// création utilisateur
 async function createUser(username, password, role='student') {
   const users = getUsers();
   const id = Date.now();
-  const hash = await bcrypt.hash(password, 10);
+  const hash = await dcodeIO.bcrypt.hash(password, 10);  // <-- ici
   users[id] = { id, username, role, passwordHash: hash };
   saveUsers(users);
 }
+
 
 async function changePassword(username, oldPassword, newPassword) {
   const users = getUsers();
   const user = Object.values(users).find(u => u.username === username);
   if (!user) return false;
 
-  const ok = await bcrypt.compare(oldPassword, user.passwordHash);
+  const ok = await dcodeIO.bcrypt.compare(oldPassword, user.passwordHash); // <-- ici
   if (!ok) return false;
 
-  user.passwordHash = await bcrypt.hash(newPassword, 10);
+  user.passwordHash = await dcodeIO.bcrypt.hash(newPassword, 10); // <-- ici
   saveUsers(users);
   return true;
 }
+
 
 const usersState = getUsers();
