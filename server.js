99d9ce7171ad76e93cd980fe586fa2a762c043ad
Mael36 <mael.beltrame@free.fr>
2026-06-19 11:50:52 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.redirect('/login.html');
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
